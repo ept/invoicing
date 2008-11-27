557c555826cbc18f3c804ce505a815133e0a1810
martin <martin@36f7a22c-cd02-4aa1-815a-27165e137ae5>
2008-11-27 23:33:42 +0000

    Made all the VAT declarations depend on the field, not just one per object (because different fields may apply different rates, and hence may have to be declared differently). BugzID: 676
    git-svn-id: https://intra.eptcom.co.uk/svn/ept/bidforwine/branches/vat/vendor/plugins/invoicing@2169 36f7a22c-cd02-4aa1-815a-27165e137ae5

--- lib/ept/invoicing.rb
+++ lib/ept/invoicing.rb
@@ -1,67 +1,78 @@
 module Ept
   module Invoicing
     module ActiveRecordMethods
-      def attr_taxable(tax_logic, *attr_names)
-        attr_names.each do |attribute_name|
-          attribute_name = attribute_name.to_s
-          generate_getter(tax_logic, attribute_name)
-          generate_setter(tax_logic, attribute_name)
-          generate_error_getter(tax_logic, attribute_name)
+      def attr_taxable(tax_logic, *method_names)
+        method_names.each do |method_name|
+          method_name = method_name.to_s
+          generate_getter(tax_logic, method_name)
+          generate_setter(tax_logic, method_name)
+          generate_error_getter(tax_logic, method_name)
+          generate_declarations(tax_logic, method_name)
         end
         
-        other_methods = [:short_tax_declaration, :long_tax_declaration] + tax_logic.mixin_methods
+        other_methods = (tax_logic.respond_to?(:mixin_methods) ? tax_logic.mixin_methods : []) || []
         other_methods.each do |method_name|
           generate_method(tax_logic, method_name.to_s)
         end
       end
 
-      def generate_getter(tax_logic, attribute_name)
-        define_method attribute_name + "_taxed" do |*args|
+      def generate_getter(tax_logic, method_name)
+        define_method method_name + "_taxed" do |*args|
           @taxed_attr_values ||= {}
-          if @taxed_attr_values[attribute_name]
-            @taxed_attr_values[attribute_name]
+          if @taxed_attr_values[method_name]
+            @taxed_attr_values[method_name]
           else
-            value = send(attribute_name)
+            value = send(method_name)
             if value.nil?
               nil
             else
-              taxed = tax_logic.display_price_to_user({:model_object => self, :value => value}, *args)
+              value = value.to_f
+              taxed = tax_logic.display_price_to_user({:model_object => self, :method => method_name, :value => value}, *args)
               Ept::Invoicing::Utils.round_to_currency_precision(taxed)
             end
           end
         end
       end
     
-      def generate_setter(tax_logic, attribute_name)
-        define_method attribute_name + "_taxed=" do |value, *args|
+      def generate_setter(tax_logic, method_name)
+        define_method method_name + "_taxed=" do |value, *args|
           @taxed_attr_values ||= {}
           @taxed_attr_error  ||= {}
-          @taxed_attr_values[attribute_name] = value
+          @taxed_attr_values[method_name] = value
           
           if value.nil?
-            send(attribute_name + "=", nil)
+            send(method_name + "=", nil)
           else
             # FIXME check if value has a valid floating-point number format
             value = Ept::Invocing::Utils.round_to_currency_precision(value.to_f)
-            internal = tax_logic.input_price_from_user({:model_object => self, :value => value}, *args)
+            internal = tax_logic.input_price_from_user({:model_object => self, :method => method_name, :value => value}, *args)
             internal = Ept::Invoicing::Utils.round_to_currency_precision(internal)
-            external = tax_logic.display_price_to_user({:model_object => self, :value => internal}, *args)
+            external = tax_logic.display_price_to_user({:model_object => self, :method => method_name, :value => internal}, *args)
             external = Ept::Invoicing::Utils.round_to_currency_precision(external)
             
             if external != value
               @taxed_attr_error[fieldname] = (external > @taxed_attr_values[fieldname]) ? :high : :low
             end
           
-            @taxed_attr_values[attribute_name] = external
-            send(attribute_name + "=", internal)
+            @taxed_attr_values[method_name] = external
+            send(method_name + "=", internal)
           end
         end
       end
     
-      def generate_error_getter(tax_logic, attribute_name)
-        define_method attribute_name + "_tax_rounding_error" do
+      def generate_error_getter(tax_logic, method_name)
+        define_method method_name + "_tax_rounding_error" do
           @taxed_attr_error ||= {}
-          @taxed_attr_error[attribute_name] 
+          @taxed_attr_error[method_name] 
+        end
+      end
+      
+      def generate_declarations(tax_logic, method_name)
+        define_method method_name + "_tax_info" do |*args|
+          tax_logic.tax_info({:model_object => self, :method => method_name}, *args)
+        end
+        define_method method_name + "_tax_details" do |*args|
+          tax_logic.tax_info({:model_object => self, :method => method_name}, *args)
         end
       end
       
@@ -71,7 +82,7 @@ module Ept
         end
       end
       
-      private :generate_getter, :generate_setter, :generate_error_getter, :generate_method
+      private :generate_getter, :generate_setter, :generate_error_getter, :generate_declarations, :generate_method
     end
   end
 end
