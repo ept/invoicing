04bf345c777e6268cff8b7f6cccb1d2d5df5ebb8
martin <martin@36f7a22c-cd02-4aa1-815a-27165e137ae5>
2008-11-30 23:51:23 +0000

    Fix bidding BugzID: 676
    git-svn-id: https://intra.eptcom.co.uk/svn/ept/bidforwine/trunk/vendor/plugins/invoicing@2186 36f7a22c-cd02-4aa1-815a-27165e137ae5

--- lib/ept/invoicing.rb
+++ lib/ept/invoicing.rb
@@ -10,6 +10,9 @@ module Ept
           generate_declarations(tax_logic, method_name)
         end
         
+        generate_before_validation(tax_logic)
+        before_validation :convert_taxed_attributes
+        
         other_methods = (tax_logic.respond_to?(:mixin_methods) ? tax_logic.mixin_methods : []) || []
         other_methods.each do |method_name|
           generate_method(tax_logic, method_name.to_s)
@@ -35,28 +38,9 @@ module Ept
       end
     
       def generate_setter(tax_logic, method_name)
-        define_method method_name + "_taxed=" do |value, *args|
+        define_method method_name + "_taxed=" do |value|
           @taxed_attr_values ||= {}
-          @taxed_attr_error  ||= {}
           @taxed_attr_values[method_name] = value
-          
-          if value.nil?
-            send(method_name + "=", nil)
-          else
-            # FIXME check if value has a valid floating-point number format
-            value = ::Ept::Invoicing::Utils.round_to_currency_precision(value.to_f)
-            internal = tax_logic.input_price_from_user({:model_object => self, :method => method_name, :value => value}, *args)
-            internal = ::Ept::Invoicing::Utils.round_to_currency_precision(internal)
-            external = tax_logic.display_price_to_user({:model_object => self, :method => method_name, :value => internal}, *args)
-            external = ::Ept::Invoicing::Utils.round_to_currency_precision(external)
-            
-            if external != value
-              @taxed_attr_error[method_name] = (external > @taxed_attr_values[method_name]) ? :high : :low
-            end
-          
-            @taxed_attr_values[method_name] = external
-            send(method_name + "=", internal)
-          end
         end
       end
     
@@ -73,6 +57,34 @@ module Ept
         end
         define_method method_name + "_tax_details" do |*args|
           tax_logic.tax_info({:model_object => self, :method => method_name}, *args)
+        end
+      end
+      
+      def generate_before_validation(tax_logic)
+        define_method "convert_taxed_attributes" do
+          @taxed_attr_values ||= {}
+          @taxed_attr_error  ||= {}
+          for method_name in @taxed_attr_values.keys
+            value = @taxed_attr_values[method_name]
+            
+            if value.nil?
+              send(method_name + "=", nil)
+            else
+              # FIXME check if value has a valid floating-point number format
+              value = ::Ept::Invoicing::Utils.round_to_currency_precision(value.to_f)
+              internal = tax_logic.input_price_from_user({:model_object => self, :method => method_name, :value => value})
+              internal = ::Ept::Invoicing::Utils.round_to_currency_precision(internal)
+              external = tax_logic.display_price_to_user({:model_object => self, :method => method_name, :value => internal})
+              external = ::Ept::Invoicing::Utils.round_to_currency_precision(external)
+              
+              if external != value
+                @taxed_attr_error[method_name] = (external > value) ? :high : :low
+              end
+            
+              @taxed_attr_values[method_name] = external
+              send(method_name + "=", internal)
+            end
+          end
         end
       end
       
