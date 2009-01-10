65c7bd44e491e50df8050ec5d93452dd796c2563
Martin Kleppmann <martin@eptcomputing.com>
2009-01-10 18:50:06 +0000

    More documentation

--- lib/invoicing.rb
+++ lib/invoicing.rb
@@ -1,7 +1,8 @@
-Dir.glob(File.join(File.dirname(__FILE__), 'invoicing/**/*.rb')).sort.each {|f| require f }
-
 require 'activerecord'
+
+Dir.glob(File.join(File.dirname(__FILE__), 'invoicing/**/*.rb')).sort.each {|f| require f }
 
 ActiveRecord::Base.send(:extend, Invoicing::Tax::AttrTaxable)
 ActiveRecord::Base.send(:extend, Invoicing::CachedRecord::ActMethods)
 ActiveRecord::Base.send(:extend, Invoicing::TimeDependent::ActMethods)
+ActiveRecord::Base.send(:extend, Invoicing::LedgerItem::ActMethods)
--- lib/invoicing/ledger_item.rb
+++ lib/invoicing/ledger_item.rb
@@ -1,18 +1,27 @@
 module Invoicing
-  # == Ledger item objects
+  # = Ledger item objects
   #
   # This module implements a simple ledger, i.e. the record of all of the business transactions
   # which are handled by your application. Each transaction is recorded as one +LedgerItem+ object,
   # each of which may have one of the following three types:
   #
-  # * +Invoice+ - When you send an invoice to someone (= a customer), this is a record of the fact
+  # +Invoice+::
+  #   When you send an invoice to someone (= a customer), this is a record of the fact
   #   that you have sold them something (a product, a service etc.), and how much you expect to be
   #   paid for it. An invoice can consist of a list of individual charges, but it is considered as
   #   one document for legal purposes. You can also create invoices from someone else to yourself,
   #   if you owe someone else money -- for example, if you need to pay commissions to a reseller of
   #   your application.
-  # * +CreditNote+ - This is basically an invoice with negative value; you should use it if you have
-  #   previously sent a customer an invoice with an amount which was too great
+  # +CreditNote+::
+  #   This is basically a negative invoice; you should use it if you have previously
+  #   sent a customer an invoice with an amount which was too great (i.e. you have overcharged them).
+  #   Note that the numeric value on a credit note is positive; this value is subtracted from the
+  #   amount which the recipient owes to the sender. For example, if you send a customer an invoice
+  #   with a +total_amount+ of $20 and a credit note with a +total_amount+ of $10 (not -$10!), that
+  #   means that you're asking them to pay $10.
+  # +Payment+::
+  #   This is a record of the fact that a payment has been made. It's a simple object,
+  #   in effect just saying that party A paid amount X to party B on date Y.
   #
   # == Important principles
   #
@@ -21,18 +30,20 @@ module Invoicing
   #
   # * <b>Invoices and Credit Notes</b> are the important documents for tax purposes in most
   #   jurisdictions. They record the date on which the sale is officially made, and that date
-  #   determines which tax rates apply. An invoice also represents the transfer of ownership from
+  #   determines which tax rates apply. An invoice often also represents the transfer of ownership from
   #   the supplier to the customer; for example, if you ask your customers to send payment in
   #   advance (such as 'topping up' their account), that money still belongs to your customer
   #   until the point where they have used your service, and you have charged them for your
-  #   service by sending them an invoice. 
+  #   service by sending them an invoice. You should only invoice them for what they have actually
+  #   used, then the remaining balance will automatically be retained on their account.
   # * <b>Payments</b> are just what it says on the tin -- the transfer of money from one hand
   #   to another. A payment may occur before an invoice is issued (payment in advance), or
   #   after/at the same time as an invoice is issued to settle the debt (payment in arrears, giving
   #   your customers credit). You can choose whatever makes sense for your business.
   #   Payments may often be associated one-to-one with invoices, but not necessarily -- an invoice
   #   may be paid in instalments, or several invoices may be lumped together to one payment. Your
-  #   customer may even refuse to pay some charges, in which case there is an invoice but no payment.
+  #   customer may even refuse to pay some charges, in which case there is an invoice but no payment
+  #   (until at some point you write it off as bad debt, but that's beyond our scope right now).
   #
   # Another very important principle is that once a piece of information has been added to the
   # ledger, you <b>should not modify or delete it</b>. Particularly when you have 'sent' one of your
@@ -58,7 +69,11 @@ module Invoicing
   # of accumulated charges. If you display it to users, make sure that you don't call it "invoice",
   # to avoid confusion. Only when you set it to +closed+ at the end of the month does the
   # statement become an invoice for legal purposes. Once it's +closed+, of course you mustn't add
-  # any further charges to it. 
+  # any further charges to it.
+  #
+  # Finally, please only use positive numeric values on invoices, credit notes and payments (unless
+  # you have been given other instructions by your accountant). Use the +sender_id+ and +recipient_id+
+  # fields to indicate the direction of a transaction (see below).
   #
   # == Using invoices, credit notes and payments in your application
   #
@@ -66,9 +81,9 @@ module Invoicing
   # single database table. We use <b>single table inheritance</b> to distinguish the object types:
   # this module provides a base class type <tt>Invoicing::LedgerItem::Base < ActiveRecord::Base</tt>
   # and three subclasses:
-  # * <tt>Invoicing::LedgerItem::Invoice</tt> - base class for all types of invoice,
-  # * <tt>Invoicing::LedgerItem::CreditNote</tt> - base class for all types of credit note, and
-  # * <tt>Invoicing::LedgerItem::Payment</tt> - base class for all types of payment.
+  # <tt>Invoicing::LedgerItem::Invoice</tt>::    base class for all types of invoice
+  # <tt>Invoicing::LedgerItem::CreditNote</tt>:: base class for all types of credit note
+  # <tt>Invoicing::LedgerItem::Payment</tt>::    base class for all types of payment
   #
   # You can create as many subclasses as you like of each of these three types. This provides a
   # convenient mechanism for encapsulating different types of functionality which you may need for
@@ -79,18 +94,71 @@ module Invoicing
   # and +SalesCommissionInvoice+, for example). Similarly for payments, you may have subclasses representing
   # credit card payments, cash payments, bank transfers etc.
   #
-  # Ledger methods:
+  # You must create at least one subclass of each of +Invoice+, +CreditNote+ and +Payment+ and assign
+  # them the same ActiveRecord table name (using <tt>ActiveRecord::Base#set_table_name</tt>). That
+  # database table must have a certain minimum set of columns and a few common methods, documented
+  # below (although you may rename any of them if you wish). Beyond those, you may add as many other
+  # methods and database columns as you like to support the operation of your application, provided
+  # they don't interfere with names used here.
   #
-  # identifier          # Assigned by the creditor/supplier
-  # type                # For single table inheritance
-  # status              # Invoices: open, closed, cancelled; Payment: pending, cleared, failed
-  # issue_date          # = tax point
-  # currency            # 3 letter code
+  # The following methods/database columns are <b>required</b> for +LedgerItem+ objects:
+  #
+  # +type+::
+  #   String to store the class name, for ActiveRecord single table inheritance.
+  #
+  # +sender_id+::
+  #   Integer-valued foreign key, used to refer to some other model object representing the party
+  #   (person, company etc.) who is the sender of the transaction.
+  #   - In the case of an invoice or credit note, the +sender_id+ identifies the supplier of the product or service,
+  #     i.e. the person who is owed the amount specified on the invoice, also known as the creditor.
+  #   - In the case of a payment record, the +sender_id+ identifies the payee, i.e. the person who sends the note
+  #     confirming that they received payment.
+  #   - This field may be +NULL+ to refer to yourself (i.e. the company/person who owns or
+  #     operates this application), but you may also use non-+NULL+ values to refer to yourself. It's just
+  #     important that you consistently refer to the same party by the same value in different ledger items.
+  #
+  # +recipient_id+::
+  #   The counterpart to +sender_id+: foreign key to a model object which represents the
+  #   party who is the recipient of the transaction.
+  #   - In the case of an invoice or credit note, the +recipient_id+ identifies the customer/buyer of the product or
+  #     service, i.e. the person who owes the amount specified on the invoice, also known as the debtor.
+  #   - In the case of a payment record, the +recipient_id+ identifies the payer, i.e. the recipient of the
+  #     payment receipt.
+  #   - +NULL+ may be used as in +sender_id+.
+  #
+  # +sender_details+::
+  #   A method (does not have to be a database column) which returns a hash with information
+  #   about the party identified by +sender_id+. See below for the expected contents of the hash. Must always return
+  #   valid details, even if +sender_id+ is +NULL+.
+  #
+  # +recipient_details+::
+  #   A method (does not have to be a database column) which returns a hash with information
+  #   about the party identified by +recipient_id+. See below for the expected contents of the hash. Must always
+  #   return valid details, even if +recipient_id+ is +NULL+.
+  #
+  # +identifier+::
+  #   A number or string used to identify this record, i.e. the invoice number, credit note number or
+  #   payment receipt number as appropriate.
+  #   - There may be legal requirements in your country concerning its format, but as long as it uniquely identifies
+  #     the document within your organisation you should be safe.
+  #   - It's possible to simply make this an alias of the primary key, but it's strongly recommended that you use a
+  #     separate database column. If you ever need to generate invoices on behalf of other people (i.e. where
+  #     +sender_id+ is not you), you need to give the sender of the invoice the opportunity to enter their own
+  #     +identifier+ (because it then must be unique within the sender's organisation, not yours).
+  #
+  # +issue_date+::
+  #   A datetime column which indicates the date on which the document is issued, and which also
+  #   serves as the tax point (the date which determines which tax rate is applied). This should be a separate
+  #   column, because it won't necessarily be the same as +created_at+ or +updated_at+. There may be business
+  #   reasons for choosing particular dates, but the date at which you send the invoice or receive the payment
+  #   should do unless your accountant advises you otherwise.
+  #
+  # +currency+::
+  #   The 3-letter code which identifies the currency used in this transaction; must be one of the list
+  #   of codes in ISO-4217[http://en.wikipedia.org/wiki/ISO_4217].
+  #
   # total_amount        # including all taxes and charges
-  # sender_id           # for grouping
-  # sender_details      # Invoice/CN: supplier; Payment: payee
-  # recipient_id        # for grouping
-  # recipient_details   # Invoice/CN: buyer; Payment: payer
+  # status              # Invoices: open, closed, cancelled; Payment: pending, cleared, failed
   # is_debit?           # false => is credit
   # is_visible?         # visible to user?
   # note/description
@@ -144,22 +212,27 @@ module Invoicing
     # Use <tt>Invoicing::LedgerItem::Invoice</tt>, <tt>Invoicing::LedgerItem::CreditNote</tt>
     # and <tt>Invoicing::LedgerItem::Payment</tt> instead.
     class Base < ::ActiveRecord::Base
-      acts_as_ledger_item
+      #acts_as_ledger_item
     end
     
     # Base class for all types of invoice in your application.
     class Invoice < Base
-      acts_as_ledger_item :subtype => :invoice
+      #acts_as_ledger_item :subtype => :invoice
     end
     
     # Base class for all types of credit note in your application.
     class CreditNote < Base
-      acts_as_ledger_item :subtype => :credit_note
+      #acts_as_ledger_item :subtype => :credit_note
     end
     
     # Base class for all types of payment note in your application.
+    # Please note that this class doesn't implement any particular means of payment (such as credit
+    # card handling) -- the purpose of this class is to record the fact that a payment has taken
+    # place, not to deal with the actual mechanics of it. However, if you want to add support for
+    # credit card handling and implement it in a subclass of +Payment+, you're more than welcome,
+    # of course.
     class Payment < Base
-      acts_as_ledger_item :subtype => :payment
+      #acts_as_ledger_item :subtype => :payment
     end
     
     
