4d7b7350f875405b749eb2e6791e7350f7a4e524
Martin Kleppmann <martin@eptcomputing.com>
2009-01-22 13:02:11 +0000

    Removed now obsolete module

--- lib/invoicing/utils.rb
+++
@@ -1,9 +0,0 @@
-module Invoicing
-  class Utils
-    def self.round_to_currency_precision(value)
-      return nil if value.nil?
-      # FIXME make rounding precision configurable, depending on currency
-      (value * 100.0).round / 100.0
-    end
-  end
-end
