054ed5925863fb76dd13ef3d508d494885c358e6
Martin Kleppmann <martin@eptcomputing.com>
2009-01-24 16:00:06 +0000

    DRY'd up invoicing.rb

--- lib/invoicing.rb
+++ lib/invoicing.rb
@@ -3,8 +3,7 @@ require 'activerecord'
 require 'invoicing/class_info'  # load first because other modules depend on this
 Dir.glob(File.join(File.dirname(__FILE__), 'invoicing/**/*.rb')).sort.each {|f| require f }
 
-ActiveRecord::Base.send(:extend, Invoicing::Tax::AttrTaxable)
-ActiveRecord::Base.send(:extend, Invoicing::CachedRecord::ActMethods)
-ActiveRecord::Base.send(:extend, Invoicing::TimeDependent::ActMethods)
-ActiveRecord::Base.send(:extend, Invoicing::LedgerItem::ActMethods)
-ActiveRecord::Base.send(:extend, Invoicing::CurrencyValue::ActMethods)
+# Mix all modules Invoicing::*::ActMethods into ActiveRecord::Base as class methods
+Invoicing.constants.map{|c| Invoicing.const_get(c) }.select{|m| m.is_a?(Module) && m.const_defined?('ActMethods') }.each{
+  |m| ActiveRecord::Base.send(:extend, m.const_get('ActMethods'))
+}
