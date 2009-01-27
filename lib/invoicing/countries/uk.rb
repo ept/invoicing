ec3dae52ae5925ed68f8ab063c84c7a614170502
Martin Kleppmann <martin@eptcomputing.com>
2009-01-27 20:53:21 +0000

    Simplistic UK VAT implementation

--- lib/invoicing/countries/uk.rb
+++ lib/invoicing/countries/uk.rb
@@ -1,6 +1,24 @@
 module Invoicing
   module Countries
     module UK
+      # Extremely simplistic implementation of UK VAT. This needs to be fixed.
+      class VAT
+        def apply_tax(params)
+          params[:value] * BigDecimal('1.15')
+        end
+        
+        def remove_tax(params)
+          params[:value] / BigDecimal('1.15')
+        end
+        
+        def tax_info(params)
+          "(inc. VAT)"
+        end
+        
+        def tax_details(params)
+          "(including VAT at 15%)"
+        end
+      end
     end
   end
 end
