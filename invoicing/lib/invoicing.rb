737ab9ee2417ffa9005d020e15cd0037818c92ff
Martin Kleppmann <martin@eptcomputing.com>
2009-04-26 14:52:40 +0100

    Undo commit 0616e068 - needed for unit testing

--- invoicing/lib/invoicing.rb
+++ invoicing/lib/invoicing.rb
@@ -1,3 +1,6 @@
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
 require 'activerecord'
 
 require 'invoicing/class_info'  # load first because other modules depend on this
