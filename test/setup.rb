6055ebd54f0a22c5a811c52198ce5827d2e5ffc8
Deepak Kannan <kannan.deepak@gmail.com>
2013-10-26 17:06:16 +0530

    [#issue-22] remove dependency on hoe
    hoe was being used to
    - Martin Kleppmann's email is configured there which is
      not specified in gemspec
    - run tests
    - publish gems etc
    
    have to ping Martin about which email is to be used
    
    for running tests, added minitest autorun and pride
    before and after checked that we run same number of tests
    ie. 197 tests, 344 assertions

--- test/setup.rb
+++
@@ -1,7 +0,0 @@
-# This file is silently executed before the entire test suite runs, when run by 'rake test'.
-# To see its output, set the environment variable VERBOSE=1
-
-require File.join(File.dirname(__FILE__), "test_helper.rb")
-
-fixtures_path = File.expand_path("../fixtures", __FILE__)
-Dir.glob(fixtures_path + "/*.rb").each { |f| require f }
